c18d431a8b6b283f6e7c0e5a3e6f2c868d5e53dd
kiam001 <blablatupfen@gmail.com>
2020-12-06 14:07:11 +0100

    Update omni_PL2.mk

--- omni_PL2.mk
+++ omni_PL2.mk
@@ -15,8 +15,6 @@
 #
 
 # Inherit some common AOSP stuff
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 
 #nherit from PL2 device
